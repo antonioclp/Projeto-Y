e7daebbdbe5f29056b4f637b5ac4b2cd8532f4c0
Antônio Santana <antonionetosg6@gmail.com>
2023-12-30 20:31:16 -0300

    Implementação de login

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// Branch - login/impl--login
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
