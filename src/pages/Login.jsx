3c98dc1b9ddad97eba7a4a4c6cee7235f1ec6dc9
Antônio Santana <antonionetosg6@gmail.com>
2023-12-30 21:13:49 -0300

    feat: adição de elementos na página de login e criação da função getDate

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+// Utils
+import { getDate } from "../utils/GetDate";
 
 export default function Login() {
+  const [date, setDate] = useState();
+
+  useEffect(() => {
+    setDate(getDate);
+  }, []);
+
   return (
     <main>
-      <span>Inicio</span>
+      <form>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="password" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="date" min="1925-01-01" max={date} />
+        </div>
+      </form>
     </main>
   );
 }
