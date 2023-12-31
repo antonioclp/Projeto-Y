9ac6c8a0c578928a069e8850bc32c56ae652d1c7
Antônio Santana <antonionetosg6@gmail.com>
2023-12-30 21:26:06 -0300

    refactor::fix: adiciona elementos necessários e deleta arquivo anterior

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,20 @@
-import React, { useEffect, useState } from "react";
-
-// Utils
-import { getDate } from "../utils/GetDate";
+import React from "react";
 
 export default function Login() {
-  const [date, setDate] = useState();
-
-  useEffect(() => {
-    setDate(getDate);
-  }, []);
-
   return (
     <main>
       <form>
         <div>
-          <label htmlFor=""></label>
-          <input type="text" />
+          <label htmlFor="inputEmail">Email</label>
+          <input type="text" id="inputEmail" name="email" />
         </div>
         <div>
-          <label htmlFor=""></label>
-          <input type="text" />
+          <label htmlFor="inputPassword">Password</label>
+          <input type="password" id="inputPassword" name="password" />
         </div>
         <div>
-          <label htmlFor=""></label>
-          <input type="password" />
-        </div>
-        <div>
-          <label htmlFor=""></label>
-          <input type="date" min="1925-01-01" max={date} />
+          <label></label>
+          <button>Login</button>
         </div>
       </form>
     </main>
