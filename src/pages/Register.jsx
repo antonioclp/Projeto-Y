7b18e1b27ddac16fc8d1c2b24b549b79e98a64fa
Antônio Santana <antonionetosg6@gmail.com>
2024-01-02 17:46:48 -0300

    feat: finaliza a página de registro com implementação de validações e mensagens de erro

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 // Utils
 import getDate from "../utils/getDate";
+import { validateEmail, validatePassword } from "../utils/validation";
 
 // Api
 import { postUser } from "../api/request";
@@ -15,7 +16,9 @@ export default function Register() {
     age: 0,
     birthday: "",
   });
+
   const [serverStatus, setStatus] = useState(0);
+  const [errorMessages, setErrorMessages] = useState();
 
   useEffect(() => {
     setDate(getDate);
@@ -24,18 +27,43 @@ export default function Register() {
   const onChangeFunc = (event) => {
     const { target } = event;
     const { name, value } = target;
+    let errorMessage = "";
 
     if (name === "birthDay") {
       setInfo((prev) => ({
         ...prev,
         birthday: value,
-        age: Number(date.slice(0, 4)) - Number(value.slice(0, 4))
+        age: Number(date.slice(0, 4)) - Number(value.slice(0, 4)),
       }));
+
+    } else {
+      setInfo((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+
+      if (value.trim().length < 2) {
+        errorMessage = "Mínimo de 2 caracteres";
+      } else {
+        if (name === "email" && !validateEmail(value)) {
+          errorMessage = "Email inválido";
+        }
+
+        if (name === "password" && !validatePassword(value)) {
+          errorMessage = "A senha deve conter pelo menos um caractere especial";
+        }
+
+        if (name === "birthday") {
+          if (Number(value.slice(0, 4)) < 1925 || Number(value.slice(0, 4)) > Number(date.slice(0, 4))) {
+            errorMessage = "Insira uma data válida.";
+          }
+        }
+      }
     }
 
-    setInfo((prev) => ({
+    setErrorMessages((prev) => ({
       ...prev,
-      [name]: value
+      [name]: errorMessage,
     }));
   };
 
@@ -55,10 +83,16 @@ export default function Register() {
         <div>
           <label htmlFor="inputName">Username</label>
           <input onChange={onChangeFunc} type="text" id="inputName" name="name" />
+          {
+            errorMessages && <span>{errorMessages.name}</span>
+          }
         </div>
         <div>
           <label htmlFor="inputEmail">Email</label>
           <input onChange={onChangeFunc} type="text" id="inputEmail" name="email" />
+          {
+            errorMessages && <span>{errorMessages.email}</span>
+          }
         </div>
         <div>
           {
@@ -68,10 +102,16 @@ export default function Register() {
         <div>
           <label htmlFor="inputPassword">Password</label>
           <input onChange={onChangeFunc} type="password" id="inputPassword" name="password" />
+          {
+            errorMessages && <span>{errorMessages.password}</span>
+          }
         </div>
         <div>
           <label htmlFor="inputDate">Birthday</label>
-          <input onChange={onChangeFunc} type="date" id="inputDate" min="1925-01-01" max={date} name="birthDay" />
+          <input onChange={onChangeFunc} type="date" id="inputDate" min="1925-01-01" max={date} name="birthday" />
+          {
+            errorMessages && <span>{errorMessages.birthday}</span>
+          }
         </div>
         <div>
           <button onClick={onClickFunc} type="button" >Confirm</button>
