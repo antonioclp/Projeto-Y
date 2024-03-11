85cf9a3c537c1b6e9867bdf52da3ed4f0f28820c
Antonio Santana <antonionetosg6@gmail.com>
2024-03-11 18:09:12 -0300

    feat: renderização das informações do usuário no perfil

--- src/components/UserCenter.jsx
+++ src/components/UserCenter.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default function UserCenter() {
+export default function UserCenter({ content }) {
+  const {
+    data: { username, email, createdDate },
+  } = content;
+
   return (
-    <div>
-      <article></article>
+    <div className="d-user">
+      <article className="d-user--artc">
+        <div className="artc__info">
+          <span>{username}</span>
+          <span>{email}</span>
+          <span>{createdDate}</span>
+        </div>
+      </article>
       <div></div>
     </div>
   );
 }
 
 UserCenter.propTypes = {
+  content: PropTypes.shape({
+    data: PropTypes.shape({
+      username: PropTypes.string,
+      email: PropTypes.string,
+      createdDate: PropTypes.string,
+    }),
+  }),
 };
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 // Components
 import { AsideLeft, AsideRight, UserCenter } from "../components";
+
+// Style
+import "../styles/pages/Profile.css";
 
 // Api
 import { getUserByUsername } from "../api/request";
@@ -10,16 +13,19 @@ import { getUserByUsername } from "../api/request";
 export default function Profile() {
   const navigate = useNavigate();
 
+  const [content, setContent] = useState({});
+
   useEffect(() => {
     const credentials = JSON.parse(localStorage.getItem("dXNlcg"));
 
     if (credentials) {
       const { username, token } = credentials;
-
-      setTimeout(async () => {
+      
+      const fetchApi = async () => {
         const data = await getUserByUsername(username, token);
-        console.log(data);
-      }, 2000);
+        setContent(data);
+      };
+      fetchApi();
     } else {
       navigate("/login");
     }
@@ -29,7 +35,7 @@ export default function Profile() {
     <main className="m-profile">
       <div className="m-profile--center">
         <AsideLeft />
-        <UserCenter />
+        {content !== null && <UserCenter content={content} />}
         <AsideRight />
       </div>
     </main>
